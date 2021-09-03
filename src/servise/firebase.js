8aa8990f99dd6ebff3616e63a0ecb68cdcd72390
50Start <74140514+50start@users.noreply.github.com>
2021-09-03 21:09:45 +0900

    Update firebase.js

--- src/servise/firebase.js
+++ src/servise/firebase.js
@@ -2,14 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyB2MvwpwYHny8reCTVxRh-02th52OhNPOw",
-  authDomain: "react-todo-pwa-1165f.firebaseapp.com",
-  projectId: "react-todo-pwa-1165f",
-  storageBucket: "react-todo-pwa-1165f.appspot.com",
-  messagingSenderId: "858773499798",
-  appId: "1:858773499798:web:d69fa4cdaf59c8f199b7b7",
-});
+
 
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
